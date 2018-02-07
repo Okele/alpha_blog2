4c9669f617dd63d29c8490e2f2e48d7cb480bc00
Okele <okele573@gmail.com>
2018-02-07 01:49:38 +0000

    updated gemfile.lock

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  get 'alpha_blog/home', to: 'alpha_blog#home'
-  get 'alpha_blog/about', to: 'alpha_blog#about'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
